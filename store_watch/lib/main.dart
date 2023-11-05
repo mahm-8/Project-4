161e9f41a0c16d0aefd2f53ad7eaeff74c486e6b
lujain <lujain@Lujains-MacBook-Pro.local>
2023-11-05 09:02:32 +0300

    update

--- store_watch/lib/main.dart
+++ store_watch/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:store_watch/screens/navi_bar.dart';
 import 'package:store_watch/screens/signup_screen.dart';
+
 void main() {
   runApp(const MainApp());
 }
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
--- store_watch/lib/screens/navi_bar.dart
+++ store_watch/lib/screens/navi_bar.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-
 import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 import 'package:store_watch/screens/home_screen.dart';
 import 'package:store_watch/screens/order_screen.dart';
